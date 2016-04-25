6b0cc5199efc10e01f6473652ab3500c472ad7bc
Alexander Oliver <alexanderdoliver@gmail.com>
2016-04-25 17:06:19 -0600

    Alex Update Part 3
    I figured we could just put a letter array in the hangman file, rather than a word array which would be huge, and possibly crash the program in times of low memory. Now we have one less function unfortunately, but our letter constructors have definitions, which may compensate. Just tell me if it still works, otherwise I'll work more on it when I get home.

--- Hangman.cpp
+++ Hangman.cpp
@@ -5,115 +5,120 @@
 
 using namespace std;
 
-Hangman::Hangman()
-{
-    //ctor
+#define WORDS 4213
+
+Letter::Letter() {
+    key = NULL;
+    found = false;
+    next = NULL;
+    previous = NULL;
+};
+
+Letter::Letter( char _letter ) {
+    Letter();
+    key = _letter;
 }
 
-Hangman::~Hangman()
-{
-    //dtor
-}
+Hangman::Hangman() {}
 
-void Hangman::buildArray(){
-    int arrayPosition = 0;
-    ifstream inFile ("nounList.txt");
-    while (getline(inFile,wordArray[arrayPosition])){
-        arrayPosition++;
+Hangman::~Hangman() {}
+
+string Hangman::setUpGame() {
+    string word;
+    {
+        ifstream inFile( "nounList.txt" );
+        int random = rand() % WORDS;
+        for( int i = 0; i < random; i ++ ) {
+            getline( inFile, word );
+        }
+        inFile.close();
     }
-}
 
-string Hangman::setUpGame(int randomChoice){
-    string word = wordArray[randomChoice]; //random word from the built array
     cout<<"\nThe computer has selected a random word"<<endl;
 
-    for (int i = 0; i < word.size(); i++){ //this shows the spaces of the word
+    for( int i = 0; i < word.size(); i ++) //this shows the spaces of the word
         cout<<"_";
-    }
+    
     cout<<endl<<endl;
 
-    cout<<"You must guess the letters to complete the word"<<endl;
+    cout<<"You must guess the Letters to complete the word"<<endl;
     cout<<"You are allowed 5 incorrect guesses"<<endl;
     return word;
 }
 
-void Hangman::gameplay(letter* word){
-    letter *temp = word;
-    int wrongGuess = 5;
-    char guess;
-    bool finished = false;
-    while (wrongGuess != -1){
-        temp = word;
-        bool found = false;
-        cout<<endl<<"Guess a letter: ";
-        cin>>guess;
-        cin.clear();
-        cout<<"------------------------------"<<endl<<endl;
-        while(temp){
-            finished = true;
-            if (temp->key == guess){
-                found = true;
-                temp->found = true;
-            }
-            temp = temp->next;
-        }
-        temp = word;
-        if (found){
-            cout<<"\nNICE GUESS!!"<<endl<<endl;
-            while (temp){
-                if (!temp->found){
-                    finished = false;
-                }
-                temp = temp->next;
-            }
-            cout<<"\nWrong guesses remaining: "<<wrongGuess<<endl<<endl;
-            cout<<"==========================="<<endl<<endl;
-            temp = word;
-
-        }
-        else if (!found){
-            cout<<"\nSorry, nope."<<endl<<endl;
-            while (temp){
-                if (!temp->found){
-                    finished = false;
-                }
-                temp = temp->next;
-            }
-            temp = word;
-            wrongGuess--;
-            if (wrongGuess != -1){
-                cout<<"\nWrong guesses remaining: "<<wrongGuess<<endl<<endl;
-                cout<<"==========================="<<endl<<endl;}
-        }
-        while (temp){
-                if (temp->found){
-                    cout<<temp->key;
-                }
-                else {
-                    cout<<"_";
-                }
-            temp = temp->next;
-            }
-            cout<<endl;
-        if (finished){
-            cout<<"GREAT JOB, YOU WIN"<<endl<<endl;
-            break;
-        }
-
-    }
-
-
-}
-letter* Hangman::buildList(string word){
-    letter *head = new letter(word[0]);
-    letter *temp = head;
-    for (int i = 1; i < word.size();i++){
-        letter *add = new letter(word[i]);
-        add->previous = temp;
-        temp->next = add;
+void Hangman::buildWord( string word ) {
+    letArray = new Letter( word[ 0 ] );
+    Letter * temp = letArray;
+    for( int i = 1; i < word.size(); i ++ ) {
+        temp->next = new Letter( word[ i ] );
         temp = temp->next;
     }
     temp->next = NULL;
 
     return head;
+}
+
+void Hangman::gameplay() {
+    Letter * temp;
+    int wrongGuess = 5;
+    char guess;
+    bool finished = false;
+    while( wrongGuess != -1 && !finished ) {
+        temp = letArray;
+        bool found = false;
+        cout<<endl<<"Guess a Letter: ";
+        cin>>guess;
+        cin.clear();
+        cout<<"------------------------------"<<endl<<endl;
+        while( temp ) {
+            finished = true;
+            if( temp->key == guess ) {
+                found = true;
+                temp->found = true;
+            }
+            temp = temp->next;
+        }
+        temp = letArray;
+        if( found ) {
+            cout<<"\nNICE GUESS!!"<<endl<<endl;
+            while( temp ) {
+                if( !temp->found )
+                    finished = false;
+                temp = temp->next;
+            }
+            cout<<"\nWrong guesses remaining: "<<wrongGuess<<endl<<endl;
+            cout<<"==========================="<<endl<<endl;
+            temp = letArray;
+        } else {
+            cout<<"\nSorry, nope."<<endl<<endl;
+            while( temp ) {
+                if( !temp->found )
+                    finished = false;
+                temp = temp->next;
+            }
+            temp = letArray;
+            wrongGuess--;
+            if( wrongGuess != -1 ) {
+                cout<<"\nWrong guesses remaining: "<<wrongGuess<<endl<<endl;
+                cout<<"==========================="<<endl<<endl;
+            }
+        }
+        while( temp ) {
+            if( temp->found )
+                cout<<temp->key;
+            else
+                cout<<"_";
+            temp = temp->next;
+        }
+        cout<<endl;
+        if( finished )
+            cout<<"GREAT JOB, YOU WIN"<<endl<<endl;
+    }
+    Letter * deleter;
+    temp = letArray;
+    while( temp ) {
+        deleter = temp;
+        temp = temp->next;
+        delete deleter;
+    }
 }
