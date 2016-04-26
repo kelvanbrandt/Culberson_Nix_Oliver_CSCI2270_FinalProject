9a11f7821b7994ecead0af30a44a0e8a889b7b02
Alexander Oliver <alexanderdoliver@gmail.com>
2016-04-26 00:19:34 -0600

    Mastermind complete
    Gonna push changes, resync, then add my stuff to the main. Also, line 37
    of Hangman.cpp still says the word being printed. But I'll see if you've
    changed that.

--- Mastermind.h
+++ Mastermind.h
@@ -1,18 +1,23 @@
 #ifndef MASTERMIND_H
 #define MASTERMIND_H
 
-enum COLOR : char { RED, GREEN, YELLOW, BLUE, ORANGE, INDIGO, VIOLET, WHITE };
+#include <string>
+
+enum COLOR : char { RED, BLUE, YELLOW, PURPLE, ORANGE, GREEN, INDIGO, WHITE };
 
 class Mastermind {
-    COLOR * solution, * guess;
-    
-    public:
-        Mastermind();
-        ~Mastermind();
-        void setDifficulty();
-        void generateSolution();
-        void print();
-        void gameplay();
+	COLOR * solution, * guess;
+	bool repeats;
+	int colors, spaces, guesses;
+	
+	public:
+		Mastermind();
+		~Mastermind();
+		void setDifficulty();
+		void generateSolution();
+		bool parceGuess( std::string );
+		int accuracy();
+		void gameplay();
 };
 
 #endif // MASTERMIND_H
